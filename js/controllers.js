6ffdc7a1f2bc31440ffc9aef63c72fe5010a38d7
zbessler@brandnetworksinc.com <zbessler@brandnetworksinc.com>
2014-08-16 14:41:03 -0400

    Put the logo next the wines issue #4  added copy to all sections issues #3 #2

--- js/controllers.js
+++ js/controllers.js
@@ -2,28 +2,14 @@
 
 angular.module('Spice.controllers', [])
   .controller('Main', ['$scope','$routeParams', function($scope, $routeParams) {
+    $scope.welcomeHeader = 'Welcome to Spice Boys Vintning and Brewing';
+    $scope.welcomeBody = 'Drink the Spice Flow the Spice Enjoy the Spice Be the Spice';
     $scope.aboutH1 = 'ABOUT US';
-    $scope.aboutP1 = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.';
+    $scope.aboutP1 = 'The spice boys, once lead by Erica "The D" Bonte, were a group of mild mannered young men with hankering for anything with an abv above five percent. Begining with unceremoniously early exit by Eric "MasterRace" Parker, a flurry of departures, starting with Michael "Ladyboner" Bova and finally their leader. Leaving behind Zachary "Dad" Bessler to raise Andrew "Hitler" Vrankovic on his own.';
     $scope.aboutH2 = 'HISTORY';
-    $scope.aboutP2 = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.';
+    $scope.aboutP2 = 'Zachary purchased the winery site in 2013. The moment he saw it he fell in love with the "sweet angles". Zachary and Andrew quickly purchased necessary supplies and began scientifically engineer and preparing the wine and equipment until they started getting pretty drunk and doing whatever they wanted. That created the taste you know and love today.';
     $scope.aboutImg1 = 'img/boys_suits.jpg';
     Parse.initialize("rKS4FvpPwIN1sCSe9jzclbCHT7dU9Bc6yb1aYTxo", "k7xpt81zqRsx57ITWPgqsB2SL4JO2pvJLWo8j6mx");
-    var Wine = Parse.Object.extend("Wine");
-    var w = {
-      name: 'Huulllo?',
-      desc: 'Pale yellow in color, with ripe citrus notes and juicy tropical aromas. Fresh acidity with bright lemon and grapefruit flavors which are ripe and refreshing on the palate. Crisp and delightful with a smooth, medium-body and elegant finish.',
-      area: 'Valle del Rapel',
-      vineyardName: 'Casablanca Valley, Chile',
-      soil: 'Granitic with red clay, sandy loam and',
-      elevation: '900 to 1300 feet',
-      varietal: '100% Sauvignon Blanc',
-      aging: '4 months on the lees',
-      alcohol: '13.0%',
-      pH: '3.3',
-      acidity: '6.5 g/L'
-    };
-    //new Wine().save(w);
-
   }])
   .controller('Home', ['$scope','$rootScope' ,'$routeParams', function($scope, $rootScope, $routeParams) {
 
